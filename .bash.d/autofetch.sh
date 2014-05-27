0777e237740cae9c02b760ac4b553764f230ce1d
b4b4r07 <b4b4r07@gmail.com>
2014-05-27 21:25:28 +0900

    Update

--- .bash.d/autofetch.sh
+++ .bash.d/autofetch.sh
@@ -2,15 +2,25 @@
 
 FILE="$HOME/.fetch_dir"
 
-if [ -f $FILE ]; then
-	:> $FILE
+if ! type git >/dev/null 2>&1; then
+	echo "This script needs git command!" >/dev/stderr
+	exit 1
 fi
+
+# Make blank file
+:> $FILE
 
 auto_fetch()
 {
-	if [ -d $PWD/.git ]; then
-		if ! grep $PWD $FILE >/dev/null; then
-			echo "$PWD" >> $FILE
+	CLONE=$( history | tail -n 2 | head -n 1 | grep "git clone" )
+	if [ ! -z "$CLONE" ]; then 
+		echo "$PWD" >>$FILE
+		return
+	fi
+
+	if [ -d "$PWD"/.git ]; then
+		if ! grep "$PWD" $FILE >/dev/null; then
+			echo "$PWD" >>$FILE
 			git fetch
 		fi
 	fi

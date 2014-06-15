e0943bbb968e017213d537914310ad7931a22263
b4b4r07 <b4b4r07@gmail.com>
2014-06-15 23:56:15 +0900

    Update

--- .bash.d/cdhist.sh
+++ .bash.d/cdhist.sh
@@ -1,41 +1,23 @@
-#   @(#) Directory history manager on bash.
-#
-#   Name:     cdhist.sh
-#   Author:   b4b4r07 <b4b4r07@gmail.com>
-#   URL:      https://github.com/b4b4r07/cdhist
-#             (see this url for latest release & screenshots)
-#   License:  OSI approved MIT license
-#   Created:  Tue Sep 3 01:33:53 2013 +0900
-#   Modified: 
-#
-#   Copyright (c) 2013, b4b4r07
-#   All rights reserved.
-#
-###################################################################################################################
+#!/bin/sh
 
-[ "$BASH_VERSION" ] || return 1
+# Only shell script for bash
+if [ ! "$BASH_VERSION" ]; then
+	echo "Require bash"
+	exit
+fi
 
-declare -r cdhistlist=~/.cdhistlog
+# Declare and initialize a variable
+declare    cdhistlist="$HOME/.cdhistlog"
 declare -i CDHIST_CDQMAX=10
-declare -a CDHIST_CDQ
+declare -a CDHIST_CDQ=()
 
 function _cdhist_initialize() {
-	OLDIFS=$IFS
-	IFS=$'\n'
+	OLDIFS=$IFS; IFS=$'\n'
 	
-	#local -a mylist=( $( tail -r $cdhistlist ) )
 	local -a mylist=( $( cat $cdhistlist ) )
 	local -a temp=()
 	local -i i=count=0
 	
-	#for ((i=0; i<${#mylist[*]}; i++)); do
-	#	if ! echo "${temp[*]}" | grep -x "${mylist[i]}" >/dev/null; then
-	#		temp[i]="${mylist[i]}"
-	#		CDHIST_CDQ[$count]="${mylist[i]}"
-	#		let count++
-	#		[ $count -eq $CDHIST_CDQMAX ] && break
-	#	fi
-	#done
 	for ((i=${#mylist[*]}-1; i>=0; i--)); do
 		if ! echo "${temp[*]}" | grep -x "${mylist[i]}" >/dev/null; then
 			temp[$count]="${mylist[i]}"
@@ -44,7 +26,6 @@ function _cdhist_initialize() {
 			[ $count -eq $CDHIST_CDQMAX ] && break
 		fi
 	done
-	
 	IFS=$OLDIFS
 }
 
@@ -131,29 +112,69 @@ function _cdhist_back() {
 	fi
 }
 
-function cd { _cdhist_cd "$@"; }
-function +  { _cdhist_forward "$@"; }
-function -  { _cdhist_back "$@"; }
-#function =  { _cdhist_history "$@"; }
-function =  { 
-	if expr "$1" : '[0-9]*' > /dev/null ; then
-		# if 1..9
-		_cdhist_history "$@"
+function _cdhist_list() {
+	if [ -z "$1" ]; then
+		sort $cdhistlist | uniq -c | sort -nr | head | sed "s $HOME ~ g"
 	else
-		# if a..z
-		if [ $# -eq 0 ]; then
-			_cdhist_history "$@"
-		elif [ $# -eq 1 ]; then
-			if [ $(_cdhist_history | grep -i "$1" | wc -l) -eq 1 ]; then
-				cd $(_cdhist_history | grep -i "$1" | awk /$2/'{print $2}' | sed "s ~ $HOME g")
-			else
-				_cdhist_history | grep -i "$1"
-			fi
-		else
-			cd $(_cdhist_history | grep -i "$1" | awk /$2/'{print $2}' | sed "s ~ $HOME g")
-		fi
-
+		_cdhist_cd $(sort $cdhistlist | uniq -c | sort -nr | head | nl | awk '{if($1=='$1') print $3}' | sed "s ~ $HOME g")
 	fi
+}
+
+function _cdhist_find() {
+	db=$(sort $cdhistlist | uniq | \grep -i "/\.\?$1")
+	shift
+
+	for i do
+		db=$(echo "${db}" | \grep -i "/\.\?${i}")
+	done
+
+	if [ $(echo "${db}" | wc -l) -eq 1 ]; then
+		_cdhist_cd "${db}"
+	else
+		echo "${db}" | sed "s $HOME ~ g"
+	fi
+}
+
+function - {
+	_cdhist_forward "$@";
+}
+
+function + {
+	_cdhist_back "$@";
+}
+
+function = { 
+	if ( test -z "$1" || expr "$1" : '[0-9]*' || expr "`eval echo '$'{$#}`" : '[0-9]*' ) >/dev/null; then
+		_cdhist_history "$@"
+		return
+	fi
+
+	# siborikomi >>>>
+	db=$(_cdhist_history | \grep -i "/\.\?$1")
+	shift
+	for i do
+		db=$(echo "${db}" | \grep -i "/\.\?${i}")
+	done
+	# siborikomi <<<<
+
+	if [ $(echo "${db}" | wc -l) -eq 1 ]; then
+		_cdhist_cd $(echo "${db}" | awk '{print $2}' | sed "s ~ $HOME g")
+	else
+		echo "${db}" | sed "s ~ $HOME g"
+	fi
+}
+
+function cd() {
+	if [ "$1" = '-l' -o "$1" = '--most-used' ]; then
+		shift
+		_cdhist_list "$@"
+		return 0
+	elif [ "$1" = '-f' -o "$1" = '--find' ]; then
+		shift
+		_cdhist_find "$@"
+		return 0
+	fi
+	_cdhist_cd "$@"
 }
 
 if [ -f $cdhistlist ]; then
@@ -164,69 +185,12 @@ else
 	_cdhist_reset
 fi
 
-############################################################
-
-_cdhist_list ()
-{
-	shift
-	if [ -z "$1" ]; then
-		sort $cdhistlist | uniq -c | sort -nr | head | sed "s $HOME ~ g"
-	else
-		_cdhist_cd $(sort $cdhistlist | uniq -c | sort -nr | head | nl | awk '{if($1=='$1') print $3}' | sed "s ~ $HOME g")
-	fi
-}
-
-_cdhist_about ()
-{
-	local i=
-	local result_even=/tmp/result.$$
-	local result_odd=/tmp/result.$$2
-
-	sort $cdhistlist | uniq | grep -i "$1" >$result_even
-	sort $cdhistlist | uniq | grep -i "$1" >$result_odd
-	
-	shift
-	for i in "$@"
-	do
-		if [ `expr $# % 2` == 0 ]; then
-			awk '/\/'"$i"'/' $result_even >$result_odd
-		else
-			awk '/\/'"$i"'/' $result_odd >$result_even
-		fi
-		shift
-	done 
-	
-	a=`wc -l $result_even | awk '{print $1}'`
-	b=`wc -l $result_odd | awk '{print $1}'`
-	
-	if [ $a -lt $b ]; then
-		cat $result_even
-	else
-		cat $result_odd
-	fi | sed "s $HOME ~ g" | tr '\/' ' '
-	
-	command rm -r $result_even $result_odd
-}
-
-cd ()
-{
-	if [ "$1" = '-l' -o "$1" = '--most-used' ]; then
-		_cdhist_list "$@"
-		return 0
-	#elif [ "$1" = '-a' -o "$1" = '--about' ]; then
-	#	_cdhist_about "$@"
-	#	return 0
-	fi
-	_cdhist_cd "$@"
-}
-
 if [ "$enable_auto_cdls" ]; then
-	auto_cdls()
-	{
+	function _cdhist_auto_cdls() {
 		if [ "$OLDPWD" != "$PWD" ]; then
 			ls
 			OLDPWD="$PWD"
 		fi
 	}
-	PROMPT_COMMAND="$PROMPT_COMMAND"$'\n'auto_cdls
+	PROMPT_COMMAND="$PROMPT_COMMAND"$'\n'_cdhist_auto_cdls
 fi

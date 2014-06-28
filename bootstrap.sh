348a3f221b5609abd65139affdefebea3430b8a2
b4b4r07 <b4b4r07@gmail.com>
2014-06-29 00:04:56 +0900

    Extend

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,23 +15,20 @@ function doIt() {
 		make deploy;
 		cd;
 		source ~/.bash_profile
+		make help
+		echo -e "You should do '\033[33mmake install\033[m' to setup your environment."
 	fi;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
-	echo -n "Start to install...\n"
-	echo -n "Cloning dotfiles repo to your home. Are you sure? (y/n) "
-	read
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-	#read -p "Cloning dotfiles repo to your home. Are you sure? (y/n) " -n 1;
-	#echo "";
-	#if [[ $REPLY =~ ^[Yy]$ ]]; then
-	#	doIt;
-	#fi;
+	make help
+	echo ""
+	echo "By executing ./bootstrap.sh with -f option, the following commands are run."
+	echo "  1. git clone b4b4r07/dotfiles.git"
+	echo "  2. make deploy"
+	echo "  3. source .bash_profile"
 fi;
 
 unset doIt;

996c02e58d9222415fb8f02fb953b27bda1d62b4
b4b4r07 <b4b4r07@gmail.com>
2014-06-29 22:24:04 +0900

    Change header

--- init/lang.sh
+++ init/lang.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
+function change_homedir_language()
+{
+	if uname -s | grep -qi "darwin"
+	then
+		sudo -v
+	
+		# CF: http://qiita.com/is0me/items/0b7b846f1f0860629950
+		if [ $(echo "`sw_vers -productVersion |cut -d. -f1 -f2` > 10.8" | bc) -eq 1 ]; then
+			cd /System/Library/CoreServices/SystemFolderLocalizations/ja.lproj
+		else
+			cd /System/Library/CoreServices/SystemFolderLocalizations/Japanese.lproj
+		fi
+	
+		mv SystemFolderLocalizations.strings SystemFolderLocalizations.strings.back
+		cp ../en.lproj/SystemFolderLocalizations.strings .
+	
+		killall Finder
+	
+	elif uname -s | grep -qi "linux"
+	then
+		LANG=C xdg-user-dirs-gtk-update
+	fi
+}
+
 read -p "Change from Japanese to English display of directory name. Are you sure? (y/n) " -n 1
 echo ""
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-	echo "Nothing to do. To exit."
-	exit 1
-fi
-	
-if uname -s | grep -qi "darwin"
-then
-	sudo -v
-
-	# CF: http://qiita.com/is0me/items/0b7b846f1f0860629950
-	if [ $(echo "`sw_vers -productVersion |cut -d. -f1 -f2` > 10.8" | bc) -eq 1 ]; then
-		cd /System/Library/CoreServices/SystemFolderLocalizations/ja.lproj
-	else
-		cd /System/Library/CoreServices/SystemFolderLocalizations/Japanese.lproj
-	fi
-
-	mv SystemFolderLocalizations.strings SystemFolderLocalizations.strings.back
-	cp ../en.lproj/SystemFolderLocalizations.strings .
-
-	killall Finder
-
-elif uname -s | grep -qi "linux"
-then
-	LANG=C xdg-user-dirs-gtk-update
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	change_homedir_language
 fi
--- init/package.sh
+++ init/package.sh
@@ -1,54 +1,56 @@
 #!/bin/bash
 
+function install_commands_by_pacman()
+{
+	if [ "$OS" == "darwin" ]; then
+		PACMAN="brew"
+	elif [ "$OS" == "linux" ]; then
+		if [ -f /etc/issue ]; then
+			KIND=$(cat /etc/issue | cut -d' ' -f1)
+			case "$KIND" in
+				"Ubuntu"|"Debian" ) PACMAN="apt-get" ;;
+				"CentOS"|"Fedora" ) PACMAN="yum" ;;
+				* ) PACMAN=""
+			esac
+		fi
+	fi
+	
+	COMMANDS=(
+		'ack'
+		'bash-completion'
+		'colordiff' 
+		'cowsay'
+		'figlet'
+		'fortune'
+		'gawk'
+		'gist'
+		'gisty'
+		'hub'
+		'tig'
+		'tree'
+		'vim'
+		'wget'
+	);
+	
+	if [ -z "$PACMAN" ]; then
+		read -p "Enter package manager name you want to use: " PACMAN
+	fi
+	
+	for cmd in "${COMMANDS[@]}"
+	do
+		sudo "$PACMAN" install "$cmd" 2>/dev/null
+		if [ $? -ne 0 ]; then
+			failed+=("$cmd")
+		fi
+	done
+	
+	if [ $cmd ]; then
+		echo "${failed[@]}: not installed"
+	fi
+}
+
 read -p "Install the required commands using the package management system. Are you sure? (y/n) " -n 1
 echo ""
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-	echo "Nothing to do. To exit."
-	exit 1
-fi
-
-if [ "$OS" == "darwin" ]; then
-	PACMAN="brew"
-elif [ "$OS" == "linux" ]; then
-	if [ -f /etc/issue ]; then
-		KIND=$(cat /etc/issue | cut -d' ' -f1)
-		case "$KIND" in
-			"Ubuntu"|"Debian" ) PACMAN="apt-get" ;;
-			"CentOS"|"Fedora" ) PACMAN="yum" ;;
-			* ) PACMAN=""
-		esac
-	fi
-fi
-
-COMMANDS=(
-	'ack'
-	'bash-completion'
-	'colordiff' 
-	'cowsay'
-	'figlet'
-	'fortune'
-	'gawk'
-	'gist'
-	'gisty'
-	'hub'
-	'tig'
-	'tree'
-	'vim'
-	'wget'
-);
-
-if [ -z "$PACMAN" ]; then
-	read -p "Enter package manager name you want to use: " PACMAN
-fi
-
-for cmd in "${COMMANDS[@]}"
-do
-	sudo "$PACMAN" install "$cmd" 2>/dev/null
-	if [ $? -ne 0 ]; then
-		failed+=("$cmd")
-	fi
-done
-
-if [ $cmd ]; then
-	echo "${failed[@]}: not installed"
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	install_commands_by_pacman
 fi

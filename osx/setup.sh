b34edffbd1e4a509e3192bb0b89cad1e1bb351d2
b4b4r07 <b4b4r07@gmail.com>
2014-06-30 15:36:50 +0900

    Add branch about curl and wget

--- osx/setup.sh
+++ osx/setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-wget -O bttrc https://db.tt/CTHoyLng # bttrc
-wget -O popclip.tar.gz https://db.tt/V2hTAodO #popclip
-wget -O alfred.tar.gz https://db.tt/kozhDx3k #alfred
+if type curl >/dev/null 2>&1
+then
+	crul https://db.tt/CTHoyLng >bttrc            # bttrc
+	crul https://db.tt/V2hTAodO >popclip.tar.gz   # popclip
+	crul https://db.tt/kozhDx3k >alfred.tar.gz    # alfred
+
+elif type wget >/dev/null 2>&1
+then
+	wget -O bttrc https://db.tt/CTHoyLng          # bttrc
+	wget -O popclip.tar.gz https://db.tt/V2hTAodO # popclip
+	wget -O alfred.tar.gz https://db.tt/kozhDx3k  # alfred
+
+else
+	echo "You don't have downloader."
+	exit
+	
+fi
+

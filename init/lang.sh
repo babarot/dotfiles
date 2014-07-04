8172595b36912b36e13cfc437e114b3c880fa008
b4b4r07 <b4b4r07@gmail.com>
2014-07-04 20:00:28 +0900

    Change support only linux

--- init/lang.sh
+++ init/lang.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
-function change_homedir_language()
-{
-	if uname -s | grep -qi "darwin"
-	then
-		sudo -v
-	
-		# CF: http://qiita.com/is0me/items/0b7b846f1f0860629950
-		if [ $(echo "`sw_vers -productVersion |cut -d. -f1 -f2` > 10.8" | bc) -eq 1 ]; then
-			cd /System/Library/CoreServices/SystemFolderLocalizations/ja.lproj
-		else
-			cd /System/Library/CoreServices/SystemFolderLocalizations/Japanese.lproj
-		fi
-	
-		mv SystemFolderLocalizations.strings SystemFolderLocalizations.strings.back
-		cp ../en.lproj/SystemFolderLocalizations.strings .
-	
-		killall Finder
-	
-	elif uname -s | grep -qi "linux"
-	then
-		LANG=C xdg-user-dirs-gtk-update
-	fi
-}
+if ! uname -s | grep -qi "linux"; then
+	exit
+fi
 
 read -p "Change from Japanese to English display of directory name. Are you sure? (y/n) " -n 1
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	change_homedir_language
+	LANG=C xdg-user-dirs-gtk-update
 fi

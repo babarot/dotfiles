808cafa49e3836aa0a69844664df359b31a80434
Z1Z1R07 <z1z1r07@gmail.com>
2014-07-07 08:29:53 +0900

    Add y/n check

--- init/setup.sh
+++ init/setup.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-git clone https://github.com/usp-engineers-community/Open-usp-Tukubai
+function make_ruby() {
+	if (type ruby && type gem) >/dev/null 2>&1; then
+		sudo gem install rubygems-update
+		sudo update_rubygems
+		sudo gem update
+	
+		gem install rake
+		gem install tw
+	fi
+}
+
+function make_tukubai() {
+	mv -f ~/tukubai ~/tukubai
+	git clone https://github.com/usp-engineers-community/Open-usp-Tukubai ~/tukubai
+	cd ~/tukubai
+	make install
+}
+
+read -p "$0: Setup ruby. Are you sure? (y/n) " -n 1
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	make_ruby
+fi
+
+read -p "$0: Setup Open-usp-Tukubai. Are you sure? (y/n) " -n 1
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	make_tukubai
+fi

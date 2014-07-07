3f37d519d88bda34deaca1597003a857ff1c8fed
Z1Z1R07 <z1z1r07@gmail.com>
2014-07-07 20:39:09 +0900

    Fix typo

--- init/git.sh
+++ init/git.sh
@@ -8,8 +8,8 @@ fi
 
 if type curl >/dev/null 2>&1
 then
-	curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash >git-completion.bash
-	curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh       >git-prompt.sh
+	curl -L https://raw.github.com/git/git/master/contrib/completion/git-completion.bash >git-completion.bash
+	curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh       >git-prompt.sh
 
 elif type wget >/dev/null 2>&1
 then

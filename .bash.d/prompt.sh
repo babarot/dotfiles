a61b6c72bddc8b43c3cba395bf6802de190ab7b8
Z1Z1R07 <z1z1r07@gmail.com>
2014-07-07 20:36:09 +0900

    Re-newal prompt

--- .bash.d/prompt.sh
+++ .bash.d/prompt.sh
@@ -23,19 +23,20 @@ export COLOUR_HIGHLIGHT_MAGENTA="${ESC}[36;1m"
 export COLOUR_HIGHLIGHT_WHITE="${ESC}[37;1m"
 export COLOUR_DEFAULT="${ESC}[m"
 
-PS_USER="\[${COLOUR_BLUE}\]\u\[${COLOUR_DEFAULT}\]"
+PS_HOST="\[${COLOUR_GREEN}\]\h\[${COLOUR_DEFAULT}\]"
+PS_USER="\[${COLOUR_GREEN}\]\u\[${COLOUR_DEFAULT}\]"
 PS_WORK="\[${COLOUR_HIGHLIGHT_YELLOW}\]\w\[${COLOUR_DEFAULT}\]"
-#PS_HIST="\[${COLOUR_HIGHLIGHT_RED}\]\!\[${COLOUR_DEFAULT}\]"
 PS_HIST="\[${COLOUR_RED}\](\!)\[${COLOUR_DEFAULT}\]"
-
-if [ -n "${PARTTY_SESSION}" ] ; then
-	PS_HOST="\[${COLOUR_GREEN}\]${PARTTY_SESSION}!\[${COLOUR_DEFAULT}\]"
-else
-	PS_HOST="\[${COLOUR_GREEN}\]\h\[${COLOUR_DEFAULT}\]"
-fi
 
 if [ -n "${WINDOW}" ] ; then
 	PS_SCREEN="\[${COLOUR_CYAN}\]#${WINDOW}\[${COLOUR_DEFAULT}\]"
+else
+	PS_SCREEN=""
+fi
+
+if [ -n "${TMUX}" ] ; then
+    TMUX_WINDOW=$(tmux display -p '#I-#P')
+	PS_SCREEN="\[${COLOUR_CYAN}\]#${TMUX_WINDOW}\[${COLOUR_DEFAULT}\]"
 else
 	PS_SCREEN=""
 fi
@@ -45,21 +46,20 @@ if [ -n "${SSH_CLIENT}" ] ; then
 else
 	PS_SSH=""
 fi
-ATODE_FILE="${HOME}/.atode"
 
-if [ -f "${ATODE_FILE}" ]; then
-	PS_ATODE="\[${COLOUR_RED}\]:\$(cat ${ATODE_FILE} 2>/dev/null | wc -l)\[${COLOUR_DEFAULT}\]"
-else
-	PS_ATODE=""
-fi
-
-if [ -f $BASH_COMPLETION_DIR/git ]; then
-#if [ ! -z "$(type -t __git_ps1)" ]; then
-	export PS1="[${PS_USER}${PS_ATODE}@${PS_HOST}${PS_SCREEN}${PS_SSH} ${PS_HIST}:${PS_WORK}]\[\033[01;32m\]"'$(if git status &>/dev/null;then echo git[branch:$(git branch | cut -d" "  -f2-) change:$(git status -s |wc -l)];fi)\[\033[00m\]\$ '
+PS1=
+if type __git_ps1 >/dev/null 2>&1; then
+	PS1+="[${PS_USER}${PS_ATODE}@${PS_HOST}${PS_SCREEN}${PS_SSH}:${PS_WORK}]\[\033[01;32m\]"
+	PS1+='$(if git status &>/dev/null;then echo git[branch:$(git branch | cut -d" "  -f2-) change:$(git status -s |wc -l)];fi)\[\033[00m\]'
+	PS1+='$ '
 else
 	GIT_PS1_SHOWDIRTYSTATE=true
 	GIT_PS1_SHOWSTASHSTATE=true
 	GIT_PS1_SHOWUNTRACKEDFILES=true
 	GIT_PS1_SHOWUPSTREAM=auto
-	export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[33m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
+	PS_GIT="${COLOUR_RED}"'$(__git_ps1)'"${COLOUR_DEFAULT}"
+
+	PS1+="${PS_USER}@${PS_HOST}:${PS_WORK}${PS_GIT}"
+	PS1+='$ '
 fi
+export PS1;

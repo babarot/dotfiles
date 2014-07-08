0ca47e0b46b905367c05d5759fb979b46f0d49da
Z1Z1R07 <z1z1r07@gmail.com>
2014-07-08 13:53:11 +0900

    Extend

--- init/vim.sh
+++ init/vim.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-function Install_Vim_From_Source()
+function build_vim_from_source()
 {
-	INSTALL_DIR=/usr/loca/app/vim.new
+	INSTALL_DIR=/usr/local/bin/vim.new
 	sudo -v
-	sudo mkdir -p $INSTALL_DIR
-	cd $INSTALL_DIR
+	sudo mkdir -p "$INSTALL_DIR"
+	cd "$INSTALL_DIR"
 
 	if type hg >/dev/null 2>&1; then
 		hg clone https://vim.googlecode.com/hg Vim
@@ -16,60 +16,44 @@ function Install_Vim_From_Source()
 		cd vim73/
 	fi
 
-	./configure --prefix=$INSTALL_DIR --enable-multibyte --with-features=huge --enable-fontset --enable-cscope --disable-selinux --disable-gui
+	./configure --prefix="$INSTALL_DIR" \ 
+	--enable-multibyte \ 
+	--with-features=huge \ 
+	--enable-fontset \ 
+	--enable-cscope \ 
+	--disable-selinux \ 
+	--disable-gui
+
 	make 
 	make test
 	sudo make install
 }
 
-function Check_The_Vim()
+function check_vim_type()
 {
-	Huge=Normal=moreNormal=Small=
-
-	if vim --version | grep "version" | grep -q "^Huge"; then
+	Huge=
+	case "$EDITOR" in
+		*vim*) : ;;
+		*) return;;
+	esac
+	if "$EDITOR" --version | grep "version" | grep -q "^Huge"; then
 		Huge='on'
-		moreNormal='on'
-	elif vim --version | grep "version" | grep -q "^Normal"; then
-		Normal='on'
-		moreNormal='on'
-	elif vim --version | grep "version" | grep -q "^Small"; then
-		Small='on'
-	else
-		echo "Unknown vim type"
-		echo "If you had to choose one, which would you choose?"
-		echo "  tiny, small, normal, big, huge"
-		exit
 	fi
 }
 
-Check_The_Vim;
-if [ "$Small" ]; then
-	read -p "$0: Install vim from source. Are you sure? (y/n) " -n 1
+if check_vim_type && test -n "$Huge"; then
+	read -p "Build vim from source (y/n) " -n 1
 	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		Install_Vim_From_Source;
-		Check_The_Vim;
+		build_vim_from_source
 	fi
-fi
 
-if [ "$moreNormal" ]; then
 	if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
-		read -p "$0: Execute vim with +'NeoBundleInit'. Are you sure? (y/n) " -n 1
+		read -p "Launch vim with +'NeoBundleInit' (y/n) " -n 1
 		echo ""
 		if [[ $REPLY =~ ^[Yy]$ ]]; then
-			# same as vim -c "NeoBundleInit";
-			vim +"NeoBundleInit";
+			# same as vim -c "NeoBundleInit"
+			vim +"NeoBundleInit"
 		fi
 	fi
 fi
-
-#
-# References
-#
-#	http://d.hatena.ne.jp/hosikiti/20100910/1284079341
-#	http://qiita.com/srea/items/a540d1259c095ee3ba71
-#	http://qiita.com/__Attsun__/items/4d05ec5b37b1edbc288d
-#	http://qiita.com/deepriver@github/items/7a88a662a30ded96360b
-#	http://qiita.com/kinef/items/bac09fe6717ed16d7fad
-#	http://d.hatena.ne.jp/sasaplus1/20110719/1311005875
-#

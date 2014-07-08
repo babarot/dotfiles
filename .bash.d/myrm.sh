c1f0e89b11aa3a065af4fa1d4a9d0dc660434a84
Z1Z1R07 <z1z1r07@gmail.com>
2014-07-09 02:26:09 +0900

    Add myrm.sh

---
+++ .bash.d/myrm.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+myrm_log="$HOME/.safe/log"
+myrm_box="$HOME/.safe/$(date '+%Y')/$(date '+%m')/$(date '+%d')"
+
+function myrm()
+{
+	error=""
+	for path do
+		if [ -e "$path" ]; then
+			in_trash="$myrm_box/${path##*/}.$(date '+%H')_$(date '+%M')_$(date '+%S')"
+
+			if command mv -f "$path" "$in_trash"; then
+				echo -e "$(date '+%Y-%m-%d %H:%M:%S') $(cd $(dirname $path) && pwd)/${path##*/} $in_trash" >>$myrm_log
+			else
+				echo "$path: could not move to trash" >&2
+				error="YES"
+			fi
+		else
+			# It is an error to try to remove a file that does not exist.
+			echo "$path: not exist" >&2
+			error="YES"
+		fi
+	done
+
+	[ "$error" ] && return 1
+	return 0
+}
+
+[ -f $myrm_log ] || touch $myrm_log
+[ -e $myrm_box ] || mkdir -p $myrm_box || {
+	echo "$myrm_box: could not create"
+	exit 1
+}
+
+if [ "$1" = '-l' ]; then
+	shift
+	if [ -z "$1" ]; then
+		sed "s $HOME ~ g" $myrm_log | nl
+	else
+		sed "s $HOME ~ g" $myrm_log | nl | tail -n "$1"
+	fi
+	exit
+elif [ "$1" = '-b' ]; then
+	shift
+	if [ -z "$1" ]; then
+		src="$( tail -n 1 $myrm_log | awk '{print $3}' )"
+		dst="$( tail -n 1 $myrm_log | awk '{print $4}' )"
+	else
+		src="$( nl <$myrm_log | awk '{if($1=="'$1'") print $4}' | sed "s ~ $HOME g")"
+		dst="$( nl <$myrm_log | awk '{if($1=="'$1'") print $5}' | sed "s ~ $HOME g")"
+	fi
+	test "$src" || exit
+	read -p "$src? (y/n) " -n 1
+	echo ""
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		cp -p -R $dst $src
+	fi
+	exit
+fi
+
+myrm "$@"

bcf87628478f87ba4f79ad8e6e7179c3986ead12
Z1Z1R07 <z1z1r07@gmail.com>
2014-09-14 01:43:47 +0900

    Modify the entire file

--- init/build_vim.sh
+++ init/build_vim.sh
@@ -5,58 +5,57 @@ trap "exit 0" INT EXIT
 function build_vim_from_source()
 {
 	INSTALL_DIR=/usr/local/bin/vim.new
-	sudo -v
-	sudo mkdir -p "$INSTALL_DIR"
-	cd "$INSTALL_DIR"
+	mkdir -p "$INSTALL_DIR"
+	cd
 
 	if type hg >/dev/null 2>&1; then
-		hg clone https://vim.googlecode.com/hg Vim
-		cd Vim/
-	else
+		sudo hg clone https://vim.googlecode.com/hg ~/Vim
+		cd ~/Vim/
+	elif type wget >/dev/null 2>&1; then
 		wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
 		tar jxvf vim-7.3.tar.bz2 
 		cd vim73/
+	else
+		echo "You do not have a downloader."
+		return 255
 	fi
 
-	./configure --prefix="$INSTALL_DIR" \ 
-	--enable-multibyte \ 
-	--with-features=huge \ 
-	--enable-fontset \ 
-	--enable-cscope \ 
-	--disable-selinux \ 
+	./configure --prefix="$INSTALL_DIR" \
+	--enable-multibyte \
+	--with-features=huge \
+	--enable-fontset \
+	--enable-cscope \
+	--disable-selinux \
 	--disable-gui
 
 	make 
-	make test
 	sudo make install
 }
 
 function check_vim_type()
 {
-	Huge=
-	case "$EDITOR" in
-		*vim*) : ;;
-		*) return;;
-	esac
-	if "$EDITOR" --version | grep "version" | grep -q "^Huge"; then
-		Huge='on'
+	if `which vim` --version | grep "version" | grep -q "^Huge"; then
+		false
+	else
+		true
 	fi
 }
 
-if check_vim_type && test -n "$Huge"; then
+if check_vim_type; then
 	unset Huge
 
-	read -p "Build vim from source (y/n) " -n 1
+	read -p "Build huge vim from source (y/n) " -n 1
 	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		#set -e
 		build_vim_from_source
+		#set +e
 	fi
-
+else
 	if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
 		read -p "Launch vim with +'NeoBundleInit' (y/n) " -n 1
 		echo ""
 		if [[ $REPLY =~ ^[Yy]$ ]]; then
-			# same as vim -c "NeoBundleInit"
 			vim +NeoBundleInit +qall
 		fi
 	fi

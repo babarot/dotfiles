976e20165bae8693b60faae96a4f841c2e688104
Z1Z1R07 <z1z1r07@gmail.com>
2014-09-19 21:13:41 +0900

    Add renewal vimbuild

--- init/build_vim.sh
+++ init/build_vim.sh
@@ -2,36 +2,6 @@
 
 trap "exit 0" INT EXIT
 
-function build_vim_from_source()
-{
-	INSTALL_DIR=/usr/local/bin/vim.new
-	mkdir -p "$INSTALL_DIR"
-	cd
-
-	if type hg >/dev/null 2>&1; then
-		sudo hg clone https://vim.googlecode.com/hg ~/Vim
-		cd ~/Vim/
-	elif type wget >/dev/null 2>&1; then
-		wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
-		tar jxvf vim-7.3.tar.bz2 
-		cd vim73/
-	else
-		echo "You do not have a downloader."
-		return 255
-	fi
-
-	./configure --prefix="$INSTALL_DIR" \
-	--enable-multibyte \
-	--with-features=huge \
-	--enable-fontset \
-	--enable-cscope \
-	--disable-selinux \
-	--disable-gui
-
-	make 
-	sudo make install
-}
-
 function check_vim_type()
 {
 	if `which vim` --version | grep "version" | grep -q "^Huge"; then
@@ -42,14 +12,13 @@ function check_vim_type()
 }
 
 if check_vim_type; then
-	unset Huge
-
 	read -p "Build huge vim from source (y/n) " -n 1
 	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		#set -e
-		build_vim_from_source
-		#set +e
+		cd
+		./vimbuild
+		sudo mkdir -p /usr/bin
+		sudo install -m 555 vim /usr/bin/vim
 	fi
 else
 	if [ ! -d ~/.vim/bundle/neobundle.vim ]; then

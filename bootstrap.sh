91a83d1f47d16e6bc2698f76ce26b473e56b9d3e
Z1Z1R07 <z1z1r07@gmail.com>
2014-11-26 12:14:11 +0900

    Support submodule and refactor

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -u
+
 echo '';
 echo '     | |     | |  / _(_) |           ';
 echo '   __| | ___ | |_| |_ _| | ___  ___  ';
@@ -8,19 +11,26 @@ echo ' | (_| | (_) | |_| | | | |  __/\__ \ ';
 echo '  \__,_|\___/ \__|_| |_|_|\___||___/ ';
 echo '';
 
-function doIt() {
-	if [ -d ~/.dotfiles ]; then
-		mv  -f ~/.dotfiles ~/.dotfiles.old;
-	fi;
+function make_b4b4r07()
+{
+    local dotfiles=~/.dotfiles;
 
-	git clone https://github.com/b4b4r07/dotfiles.git ~/.dotfiles;
-	if [ $? -eq 0 ]; then
-		cd ~/.dotfiles;
-		make deploy;
-		echo ""; make help;
-		echo "";
-		exec /bin/bash;
-	fi;
+    if [ -d "$dotfiles" ]; then
+        #mv  -f ~/.dotfiles ~/.dotfiles.old;
+        ( cd "$dotfiles" && git pull --rebase )
+    else
+        git clone https://github.com/b4b4r07/dotfiles.git "$dotfiles";
+    fi;
+    git submodule init
+    git submodule update
+    
+    if [ $? -eq 0 ]; then
+        cd "$dotfiles";
+        make deploy;
+        echo ""; make help;
+        echo "";
+        exec ${SHELL:-/bin/bash};
+    fi;
 }
 
 msg=$(
@@ -28,21 +38,21 @@ cat <<-EOF
 By executing ./bootstrap.sh, the following commands are run
   1. git clone b4b4r07/dotfiles.git
   2. make deploy
-  3. restart your current bash
+  3. (restart your current shell)
 Otherwise, any other changes
 
 For more information, contact me
   Author; B4B4R07 <b4b4r07@gmail.com>
   GitHub; https://github.com/b4b4r07/dotfiles.git
-  
+
 EOF
 );
 
 echo "$msg";
 if [[ "$0" =~ "bootstrap.sh" ]]; then
-	echo -e "\033[33mThat this file is started directly is not recommended\033[m"
+    echo "That this file is started directly is not recommended";
+    echo "Please run:";
+    echo "  curl -L https://raw.github.com/b4b4r07/dotfiles/master/bootstrap.sh | sh";
 else
-	doIt;
+    make_b4b4r07;
 fi
-
-unset doIt;

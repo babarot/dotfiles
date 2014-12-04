d3835ce79bd30db6a1e340a9922b2dd1903dd96c
Z1Z1R07 <z1z1r07@gmail.com>
2014-12-04 23:40:30 +0900

    Modify easier to read

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,56 +3,42 @@
 set -e
 set -u
 
-echo '';
-echo '     | |     | |  / _(_) |           ';
-echo '   __| | ___ | |_| |_ _| | ___  ___  ';
-echo '  / _` |/ _ \| __|  _| | |/ _ \/ __| ';
-echo ' | (_| | (_) | |_| | | | |  __/\__ \ ';
-echo '  \__,_|\___/ \__|_| |_|_|\___||___/ ';
-echo '';
+echo ''
+echo '     | |     | |  / _(_) |           '
+echo '   __| | ___ | |_| |_ _| | ___  ___  '
+echo '  / _` |/ _ \| __|  _| | |/ _ \/ __| '
+echo ' | (_| | (_) | |_| | | | |  __/\__ \ '
+echo '  \__,_|\___/ \__|_| |_|_|\___||___/ '
+echo ''
 
-function make_b4b4r07()
-{
-    local dotfiles=~/.dotfiles;
-
-    if [ -d "$dotfiles" ]; then
-        #mv  -f ~/.dotfiles ~/.dotfiles.old;
-        ( cd "$dotfiles" && git pull --rebase )
-    else
-        git clone https://github.com/b4b4r07/dotfiles.git "$dotfiles";
-    fi;
-    git submodule init
-    git submodule update
-    
-    if [ $? -eq 0 ]; then
-        cd "$dotfiles";
-        make deploy;
-        echo ""; make help;
-        echo "";
-        exec ${SHELL:-/bin/bash};
-    fi;
-}
-
-msg=$(
+declare dotfiles=~/.dotfiles
+declare msg=$(
 cat <<-EOF
 By executing ./bootstrap.sh, the following commands are run
-  1. git clone b4b4r07/dotfiles.git
-  2. make deploy
-  3. (restart your current shell)
-Otherwise, any other changes
-
-For more information, contact me
-  Author; B4B4R07 <b4b4r07@gmail.com>
-  GitHub; https://github.com/b4b4r07/dotfiles.git
-
+\t1. git clone https://github.com/b4b4r07/dotfiles
+\t2. make deploy
+\n
+Author: b4b4r07 aka BABAROT <b4b4r07@gmail.com>
 EOF
-);
+)
 
-echo "$msg";
+echo -e "$msg";
 if [[ "$0" =~ "bootstrap.sh" ]]; then
     echo "That this file is started directly is not recommended";
-    echo "Please run:";
-    echo "  curl -L https://raw.github.com/b4b4r07/dotfiles/master/bootstrap.sh | sh";
 else
-    make_b4b4r07;
+    if [ -d "$dotfiles" ]; then
+        cd "$dotfiles" && git pull --rebase
+    else
+        git clone https://github.com/b4b4r07/dotfiles.git "$dotfiles"
+        if [ $? -eq 0 ]; then
+            cd "$dotfiles"
+            make deploy
+            echo ""; make help
+            echo ""
+            exec ${SHELL:-/bin/bash}
+        fi
+    fi
+
+    git submodule init
+    git submodule update
 fi

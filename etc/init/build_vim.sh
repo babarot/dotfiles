e0c475433f0f896d18c97542d61361756d635fc2
Z1Z1R07 <z1z1r07@gmail.com>
2014-12-12 01:30:08 +0900

    Modify automatically make build

--- etc/init/build_vim.sh
+++ etc/init/build_vim.sh
@@ -4,8 +4,27 @@ trap 'echo Error: $0: stopped' ERR
 set -e
 set -u
 
+for candidate in $(echo "$PATH" | tr ":" "\n")
+do
+    vim_candidate="$candidate/vim"
+    if [ -x "$vim_candidate" ]; then
+        if eval "$vim_candidate" --version | grep -q "+clipboard"; then
+            vim_clipboard_path="$vim_candidate"
+            unset vim_candidate
+            #break
+            exit 0
+        fi
+    fi
+done
+
 echo -n "Rebuild vim? (y/N) "
 read
 if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-    ./vimbuild
+    export PATH="../../bin:$PATH"
+    if type vimbuild >/dev/null 2>&1
+        vimbuild
+    else
+        echo "Execute vimbuild at your terminal" 1>&2
+        exit 1
+    fi
 fi

87f45be92764474105d8ff2d8d794cf588b1855e
Z1Z1R07 <z1z1r07@gmail.com>
2014-12-13 02:08:59 +0900

    Add $OSTYPE == darwin*

--- etc/init/osx/osx_defaults.sh
+++ etc/init/osx/osx_defaults.sh
@@ -4,6 +4,11 @@ trap 'echo Error: $0: stopped' ERR
 set -e
 set -u
 
+if [[ $OSTYPE != darwin* ]]; then
+    echo 'OS X only' 1>&2
+    exit 1
+fi
+
 # Dashboard {{{1
 set_dashboard_preferences()
 {

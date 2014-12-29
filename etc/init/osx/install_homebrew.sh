3d8cbd5e86687abbe812ad8f233b9e68cbc404da
b4b4r07 <b4b4r07@gmail.com>
2014-12-29 16:14:26 +0900

    Divide to setup_{brew,cask}.sh

--- etc/init/osx/install_homebrew.sh
+++ etc/init/osx/install_homebrew.sh
@@ -12,15 +12,3 @@ if ! type brew >/dev/null 2>&1; then
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
 fi
-
-echo -n 'Install Homebrew packages? (y/N/) '
-read
-if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-    bash "$(dirname "${BASH_SOURCE}")"/Brewfile
-fi
-
-echo -n 'Install Homebrew Cask packages? (y/N/) '
-read
-if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-    bash "$(dirname "${BASH_SOURCE}")"/Caskfile
-fi

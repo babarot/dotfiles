65cfd1aff631d50dce6c1e77332a13460e4cd842
b4b4r07 <b4b4r07@gmail.com>
2015-01-02 01:04:15 +0900

    Add vital.sh to libs

---
+++ etc/libs/vital.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#!/bin/bash
+#!/bin/zsh
+
+# SHELL
+is_bash() { [[ -n $BASH_VERSION ]]; }
+is_zsh() { [[ -n $ZSH_VERSION ]]; }
+
+# PLATFORM
+export SHELL_PLATFORM='unknown'
+ostype() { echo "$OSTYPE" | tr '[:upper:]' '[:lower:]'; }
+case "$(ostype)" in
+    *'linux'*)  SHELL_PLATFORM='linux' ;;
+    *'darwin'*) SHELL_PLATFORM='osx'   ;;
+    *'bsd'*)    SHELL_PLATFORM='bsd'   ;;
+esac
+is_linux() { [[ $SHELL_PLATFORM == 'linux' || $SHELL_PLATFORM == 'bsd' ]]; }
+is_osx()   { [[ $SHELL_PLATFORM == 'osx' ]]; }
+is_bsd()   { [[ $SHELL_PLATFORM == 'bsd' || $SHELL_PLATFORM == 'osx' ]]; }
+
+# Check if exists
+is_exist() { type "$1" >/dev/null 2>&1; return $?; }
+is_exists() {
+    if [[ ${1[1,1]} =~ / ]]; then
+        if [[ -e "$1" ]]; then
+            return 0
+        fi
+    else
+        if type "$1" >/dev/null 2>&1; then
+            return 0
+        fi
+    fi
+    return 1
+}
+
+# Whether first login shell
+is_login_shell() { [[ $SHLVL == 1 ]]; }
+
+# Test whether we're in a git repo
+is_git_repo() { $(git rev-parse --is-inside-work-tree &> /dev/null); }
+
+# Check if running the screen or tmux
+is_screen_running() { [ ! -z "$STY" ]; }
+is_tmux_runnning() { [ ! -z "$TMUX" ]; }
+is_screen_or_tmux_running() { is_screen_running || is_tmux_runnning; }
+shell_has_started_interactively() { [ ! -z "$PS1" ]; }
+is_ssh_running() { [ ! -z "$SSH_CONECTION" ]; }
+
+# Check if exists and sourcing file
+import() { eval "[[ -${2:-f} $1 ]]" && source "$1"; }

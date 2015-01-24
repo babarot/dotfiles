31a87a1756fd20640cbf3cf9818d6ae0e424eebf
b4b4r07 <b4b4r07@gmail.com>
2015-01-25 01:38:02 +0900

    Change format in etc/test/install_redirect_test.py

--- etc/test/install_redirect_test.py
+++ etc/test/install_redirect_test.py
@@ -7,4 +7,4 @@ url = urllib2.urlopen(URL).geturl()
 if not url == "https://raw.githubusercontent.com/b4b4r07/dotfiles/master/etc/install":
     sys.exit(1)
 
-print 'ok: %s' % URL
+print 'ok: %s <-> %s' % (URL, url)

0ea26914fff1b9ea7d4fb3aa56d9759348a9e715
b4b4r07 <b4b4r07@gmail.com>
2015-01-25 01:10:54 +0900

    Add etc/test/install_redirect_test.py

---
+++ etc/test/install_redirect_test.py
@@ -0,0 +1,10 @@
+import sys
+import urllib2
+
+URL = "http://dot.b4b4r07.com"
+url = urllib2.urlopen(URL).geturl()
+
+if not url == "https://raw.githubusercontent.com/b4b4r07/dotfiles/master/etc/install":
+    sys.exit(1)
+
+print 'ok: %s' % URL

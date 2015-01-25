6a56fcff6638efb00318f91506e3e2aaf962540d
b4b4r07 <b4b4r07@gmail.com>
2015-01-25 18:45:40 +0900

    Debugging

--- etc/test/install_deploy_test.pl
+++ etc/test/install_deploy_test.pl
@@ -8,11 +8,14 @@ use Cwd;
 #my $root = Cwd::abs_path($0 . "/../..");
 #print "$root\n";
 
-#my $root = getcwd;
+my $cwd = getcwd;
 my $root = Cwd::abs_path($FindBin::Bin . "/../..");
+
+print "$cwd\n";
 chdir $root;
 #
 if (-f "Makefile") {
+print "$cwd\n";
     my $make_list=`make list 2>/dev/null`;
     if ($? != 0) {
         exit 1;
@@ -21,16 +24,19 @@ if (-f "Makefile") {
     my @list = split(/\n/, $make_list);
     @list = map {$_ =~ s@/$@@; $_} @list;
 
-    foreach my $f (@list) {
         chdir $ENV{'HOME'};
+print "$cwd\n";
+    foreach my $f (@list) {
         my $a = "$root/$f";
         my $b = readlink("$f");
-        if ($a eq $b) {
-            print "ok: $a <-> $b\n";
-        } else {
-            print "NG: $a\n";
-            exit 1;
-        }
+        print "a: $a\n";
+        print "b: $b\n";
+        #if ($a eq $b) {
+        #    print "ok: $a <-> $b\n";
+        #} else {
+        #    print "NG: $a\n";
+        #    exit 1;
+        #}
     }
 } else {
     exit 1;

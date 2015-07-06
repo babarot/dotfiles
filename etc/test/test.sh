ac9d2ec664ee7b7571519af350e8135fe429aae0
b4b4r07 <b4b4r07@gmail.com>
2015-07-06 20:33:33 +0900

    fix sh path

--- etc/test/test.sh
+++ etc/test/test.sh
@@ -7,7 +7,7 @@ do
     bash "$i"
 done
 
-n_unit=$(find $DOTPATH/etc/test/sh -name "*_test.sh" | xargs grep "^unit[0-9]$" | wc -l | sed "s/ //g")
-n_file=$(find $DOTPATH/etc/test/sh -name "*_test.sh" | wc -l | sed "s/ //g")
+n_unit=$(find $DOTPATH/etc/test -name "*_test.sh" | xargs grep "^unit[0-9]$" | wc -l | sed "s/ //g")
+n_file=$(find $DOTPATH/etc/test -name "*_test.sh" | wc -l | sed "s/ //g")
 
 echo "Files=$n_file, Tests=$n_unit"

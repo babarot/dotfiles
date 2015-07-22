a42f888164de9d14f78ee139b9ebbaa2476cbfb4
b4b4r07 <b4b4r07@gmail.com>
2015-07-22 19:15:38 +0900

    Commonize go.sh in etc/init

---
+++ etc/init/common/go.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
+set -eu
+
+. "$DOTPATH"/etc/lib/vital.sh
+
+if has "go"; then
+    go get -u github.com/b4b4r07/gotcha
+else
+    if has "curl"; then
+        curl -L git.io/gotcha | sh
+    elif has "wget"; then
+        wget -O - git.io/gotcha | sh
+    else
+        die "require: go, curl or wget"
+    fi
+fi
+
+if has "gotcha"; then
+    cd "$DOTPATH"/etc/init/assets/go
+    if [ -f config.toml ]; then
+        gotcha --verbose
+    else
+        die "something is wrong"
+    fi
+else
+    die "gotcha: not found"
+fi

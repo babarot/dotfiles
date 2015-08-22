d91540584e0353f707f2f9a2425fb114c6f594d6
b4b4r07 <b4b4r07@gmail.com>
2015-08-22 22:55:49 +0900

    A bit of refactoring

--- etc/scripts/mov2gif.sh
+++ etc/scripts/mov2gif.sh
@@ -2,6 +2,11 @@
 
 . $DOTPATH/etc/lib/vital.sh
 
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+    echo "usage: mov2gif file [fps] [height] [weight]"
+    exit 1
+fi
+
 if ! has "ffmpeg"; then
     die "requires: ffmpeg"
     exit 1
@@ -18,12 +23,12 @@ if [ -z "$mov_file" -o ! -f "$mov_file" ]; then
     exit 1
 fi
 
-fps_rate="${2:-10}"
-gif_file="$(dirname $mov_file)"/out.gif
+fps="${2:-10}"
+gif_file="$(dirname $mov_file)"/"${mov_file%.*}"
 
 if [ -f "$gif_file" ]; then
     die "$gif_file: already exists"
     exit 1
 fi
 
-ffmpeg -i "$mov_file" -s 600x400 -pix_fmt rgb24 -r "$fps_rate" -f gif - | gifsicle --optimize=3 --delay=3 >"$gif_file"
+ffmpeg -i "$mov_file" -s "${3:-600}"x"${4:-400}" -pix_fmt rgb24 -r "$fps" -f gif - | gifsicle --optimize=3 --delay=3 >"$gif_file"

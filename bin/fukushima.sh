f84a94ad722a1ebf6fdeb3c025de611071b3726e
b4b4r07 <b4b4r07@gmail.com>
2015-08-28 12:38:49 +0900

    Remove bin/fukushima.sh

--- bin/fukushima.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-stock=~/data/fukushima/$(date +%Y/%m/%d)
-
-if [ ! -d "$stock" ]; then
-    mkdir -p "$stock"
-fi
-
-curl -L http://i.buoy.jp/pkodg/fukushima.txt >"$stock"/fukushima_$(date +%H%M%S).txt

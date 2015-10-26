6181c5fc3a0e921e8234666e2a39e3133d6bc517
b4b4r07 <b4b4r07@gmail.com>
2015-10-26 14:20:49 +0900

    Update mov2gif option

--- etc/scripts/mov2gif.sh
+++ etc/scripts/mov2gif.sh
@@ -5,10 +5,7 @@
 # For more information, see etc/README.md
 . "$DOTPATH"/etc/lib/vital.sh
 
-if [ "$1" = "-h" -o "$1" = "--help" ]; then
-    echo "usage: mov2gif file [fps] [height] [weight]"
-    exit 1
-fi
+trap 'rm -f $gif_file' ERR
 
 if ! has "ffmpeg"; then
     echo "ffmpeg: not found" 1>&2
@@ -20,26 +17,62 @@ if ! has "gifsicle"; then
     exit 1
 fi
 
-if [ -z "$1" ]; then
-    echo "too few arugments" 1>&2
+while (($# > 0))
+do
+    case "$1" in
+        -h|--help)
+            echo "${0##*/} [OPTIONS] file" 1>&2
+            echo "  make gif file from mov file" 1>&2
+            echo "" 1>&2
+            echo "  OPTIONS:" 1>&2
+            echo "    -h, --help  show this help" 1>&2
+            echo "    -s, --size  specify gif file size e.g., 600x400" 1>&2
+            echo "    -r, --rate  frame rate per second" 1>&2
+            exit 1
+            ;;
+        -r|--rate)
+            if [[ $2 =~ ^[0-9]+$ ]]; then
+                rate="$2"; shift
+            else
+                echo "$2: invalid argument" 1>&2
+                exit 1
+            fi
+            ;;
+        -s|--size)
+            if [[ $2 =~ ^[0-9]+x[0-9]+$ ]]; then
+                size="$2"; shift
+            else
+                echo "$2: invalid argument" 1>&2
+                exit 1
+            fi
+            ;;
+        *)
+            mov_file="${mov_file:-$1}"
+            ;;
+    esac
+
+    shift
+done
+
+if [ -z "$mov_file" ]; then
+    echo "too few arguments" 1>&2
     exit 1
 fi
-mov_file="$1"
 
-if [ -z "$mov_file" -o ! -f "$mov_file" ]; then
+if [ ! -f "$mov_file" ] || [[ ! $mov_file =~ \.mov$ ]]; then
+    echo "$mov_file: no such *.mov file" 1>&2
     exit 1
 fi
 
-fps="${2:-10}"
 out_file="$(basename "$mov_file")"
-gif_file="$(dirname "$mov_file")"/"${out_file%.*}".gif
+gif_file="$(dirname "$mov_file")/${out_file%.*}.gif"
 
 if [ -f "$gif_file" ]; then
     echo "$gif_file: already exists" 1>&2
     exit 1
 fi
 
-ffmpeg -i "$mov_file" -s "${3:-600}"x"${4:-400}" -pix_fmt rgb24 -r "$fps" -f gif - | gifsicle --optimize=3 --delay=3 >"$gif_file"
-if [ $? -eq 0 ]; then
-    echo "Created $gif_file"
+ffmpeg -i "$mov_file" -s "${size:-600x400}" -pix_fmt rgb24 -r "${rate:-10}" -f gif - | gifsicle --optimize=3 --delay=3 >"$gif_file"
+if [ $? -eq 0 ] && [ -f "$gif_file" ]; then
+    echo "Created $gif_file, successfully"
 fi

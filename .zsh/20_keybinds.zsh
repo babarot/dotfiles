d431fe4b5df805c5a288ae9b26b48f6336192860
b4b4r07 <b4b4r07@gmail.com>
2015-11-04 19:24:59 +0900

    (command-selector) add comment system and accept-line system :blush:

--- .zsh/20_keybinds.zsh
+++ .zsh/20_keybinds.zsh
@@ -216,12 +216,13 @@ command-selector() {
 
     [[ ! -f $command_file || ! -s $command_file ]] && return
 
-    local cmd q k res
-    while cmd="$(
+    local cmd q k res accept
+    while accept=0; cmd="$(
         cat <$command_file \
             | sed -e '/^#/d;/^$/d' \
             | perl -pe 's/^(\[.*?\]) (.*)$/\033[31m$1\033[m\t$2/' \
             | perl -pe 's/^(: ?)(.*)$/$1\033[30;47;1m$2\033[m/' \
+            | perl -pe 's/^(.*)([[:blank:]]#[[:blank:]]?.*)$/$1\033[30;1m$2\033[m/' \
             | fzf --ansi --multi --no-sort --tac --query="$q" \
             --print-query --expect=ctrl-v --exit-0
             )"; do
@@ -233,6 +234,10 @@ command-selector() {
             vim "$command_file" < /dev/tty > /dev/tty
         else
             cmd="$(perl -pe 's/^(\[.*?\])\t(.*)$/$2/' <<<"$res")"
+            if [[ $cmd =~ "!$" || $cmd =~ "! *#.*$" ]]; then
+                accept=1
+                cmd="$(sed -e 's/!.*$//' <<<"$cmd")"
+            fi
             break
         fi
     done
@@ -242,6 +247,9 @@ command-selector() {
         BUFFER="$(tr -d '@' <<<"$cmd" | perl -pe 's/\n/; /' | sed -e 's/; $//')"
         len="${cmd%%@*}"
         CURSOR=${#len}
+        if [[ $accept -eq 1 ]]; then
+            zle accept-line
+        fi
     fi
     #zle reset-prompt
     zle redisplay

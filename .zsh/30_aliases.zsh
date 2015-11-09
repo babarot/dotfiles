44cf2372d8be74dedd5d911061d5554f8ae4110f
b4b4r07 <b4b4r07@gmail.com>
2015-11-09 14:26:22 +0900

    Add finder shell function

--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -325,3 +325,58 @@ uniq_alias() {
     awk $opt '!a[$'"${f:-0}"']++'
 }
 alias -g U="| uniq_alias"
+
+finder() {
+    local cmd q k res
+    local line ok make_dir i arr
+    local get_styles styles style
+    while : ${make_dir:=0}; ok=("${ok[@]:-dummy_$RANDOM}"); cmd="$(
+            fzf --ansi --multi --query="$q" \
+                --no-sort --prompt="[finder]> " \
+            --print-query --expect=ctrl-v,ctrl-x,ctrl-l,ctrl-q,ctrl-r,ctrl-z
+            )"; do
+        q="$(head -1 <<< "$cmd")"
+        k="$(head -2 <<< "$cmd" | tail -1)"
+        res="$(sed '1,2d;/^$/d' <<< "$cmd")"
+        [ -z "$res" ] && continue
+        case "$k" in
+            ctrl-l)
+                export LESS='-R -f -i -P ?f%f:(stdin). ?lb%lb?L/%L.. [?eEOF:?pb%pb\%..]'
+                arr=("${(@f)res}")
+                if [[ -d ${arr[1]} ]]; then
+                    ls -l "${(@f)res}" < /dev/tty | less > /dev/tty
+                else
+                    if has "pygmentize"; then
+                        get_styles="from pygments.styles import get_all_styles
+                        styles = list(get_all_styles())
+                        print('\n'.join(styles))"
+                        styles=( $(sed -e 's/^  *//g' <<<"$get_styles" | python) )
+                        style=${${(M)styles:#solarized}:-default}
+                        export LESSOPEN="| pygmentize -O style=$style -f console256 -g %s"
+                    fi
+                    less "${(@f)res}" < /dev/tty > /dev/tty
+                fi
+                ;;
+            ctrl-x)
+                if [[ ${(j: :)ok} == ${(j: :)${(@f)res}} ]]; then
+                    eval '${${${(M)${+commands[gomi]}#1}:+gomi}:-rm} "${(@f)res}" 2>/dev/null'
+                    ok=()
+                else
+                    ok=("${(@f)res}")
+                fi
+                ;;
+            ctrl-v)
+                vim -p "${(@f)res}" < /dev/tty > /dev/tty
+                ;;
+            ctrl-q)
+                echo "$res" < /dev/tty > /dev/tty
+                return $status
+                ;;
+            *)
+                echo "${(@f)res}"
+                break
+                ;;
+        esac
+    done
+}
+alias -g FD='$(finder)'

9994ab4d78de408b516a5609326f145888e1bc2b
b4b4r07 <b4b4r07@gmail.com>
2015-11-10 01:32:37 +0900

    Fmt

--- .zsh/20_keybinds.zsh
+++ .zsh/20_keybinds.zsh
@@ -114,7 +114,12 @@ bindkey '^D' _delete-char-or-list-expand
 # Ctrl-R
 _peco-select-history() {
     if true; then
-        BUFFER="$(history 1 | sort -k1,1nr | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' | fzf --query "$LBUFFER")"
+        BUFFER="$(
+        history 1 \
+            | sort -k1,1nr \
+            | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' \
+            | fzf --query "$LBUFFER"
+        )"
 
         CURSOR=$#BUFFER
         #zle accept-line

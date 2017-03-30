f0ee10bffe8ec5e04c16b17255fda961fa6c8507
b4b4r07 <b4b4r07@gmail.com>
2017-03-30 20:16:00 +0900

    Update zplug.zsh

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -110,7 +110,6 @@ zplug 'b4b4r07/zplug-cd', lazy:yes
 zplug 'b4b4r07/zplug-rm', lazy:yes
 
 zplug 'b4b4r07/tmux-powertools', \
-    ignore:init.zsh, \
     hook-load:'tmux-loader'
 
 zplug 'b4b4r07/git-powertools', \
@@ -151,8 +150,8 @@ zplug 'b4b4r07/ltsv.sh', \
     use:'(ltsv).sh', \
     rename-to:'$1'
 
-zplug 'b4b4r07/zsh-history', \
-    defer:3, use:init.zsh
+zplug 'b4b4r07/zsh-history', defer:3, use:init.zsh
+zplug 'b4b4r07/zsh-history', as:command, use:misc/fzf-wrapper.zsh, rename-to:ff
 
 if zplug check 'b4b4r07/zsh-history'; then
     export ZSH_HISTORY_FILE="$HOME/.zsh_history.db"

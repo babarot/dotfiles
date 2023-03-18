c3da13fc27b4aad165487043b894c7c69c6e343f
b4b4r07 <b4b4r07@gmail.com>
2023-03-18 09:38:30 +0900

    Add gh.sh

---
+++ etc/scripts/gh.sh
@@ -0,0 +1,7 @@
+gh extension install kawarimidoll/gh-q
+gh extension install dlvhdr/gh-dash
+gh extension install seachicken/gh-poi
+gh extension install yusukebe/gh-markdown-preview
+gh extension install mislav/gh-branch
+gh extension install heaths/gh-label
+gh extension install johnmanjiro13/gh-bump

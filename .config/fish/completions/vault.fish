512ddddc927735042b0ee03fbf0d6bb91165c62e
TheBranchDriftCatalyst <djdanielsh@gmail.com>
2024-09-08 10:20:37 -0600

    misc updates for provisioning of new system

--- .config/fish/completions/vault.fish
+++
@@ -1,9 +0,0 @@
-
-function __complete_vault
-    set -lx COMP_LINE (string join ' ' (commandline -o))
-    test (commandline -ct) = ""
-    and set COMP_LINE "$COMP_LINE "
-    /usr/local/bin/vault
-end
-complete -c vault -a "(__complete_vault)"
-

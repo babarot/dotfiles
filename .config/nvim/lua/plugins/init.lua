453c9be3cc0b118b689122cdc75360c14ffa4c0a
babarot <b4b4r07@gmail.com>
2025-02-03 15:24:39 +0900

    Update

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -71,6 +71,27 @@ require('lazy').setup({
   },
   -- Development
   {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
+  {
     'nvim-treesitter/nvim-treesitter',
     commit = '05df88ebaa94d30d682d076244615786d9e7c1a5',
     lazy = true,
